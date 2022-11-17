dd2e5d41a7b82aaf1ff443583cc1abe4213ca66f
Dmitrii <saveliy.kramarov666@gmail.com>
2022-11-17 16:51:29 +0200

    new commit

--- Test_API/src/test/java/com/aimporsoft/api/APIResponse.java
+++ Test_API/src/test/java/com/aimporsoft/api/APIResponse.java
@@ -1,16 +1,16 @@
 package com.aimporsoft.api;
 
-public class SuccessReg {
+public class APIResponse {
     private Integer code;
     public String type;
     private String message;
 
-    public SuccessReg(Integer code, String message) {
+    public APIResponse(Integer code, String message) {
         this.code = code;
         this.message = message;
     }
 
-    public SuccessReg() {
+    public APIResponse() {
     }
 
     public Integer getCode() {
